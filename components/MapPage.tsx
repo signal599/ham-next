ff604ffd98f60c8a50764c8883f322b1bbf8d9f4
Ross Keatinge <ross@earthbubble.com>
2026-04-02 08:31:40 -0400

    Styling

--- components/MapPage.tsx
+++ components/MapPage.tsx
@@ -89,7 +89,7 @@ export default function MapPage({ initialQuery }: Props) {
   }
 
   return (
-    <div className="flex flex-col gap-6 p-4 -m-5">
+    <div className="flex flex-col gap-6 -mt-4">
       <div className="flex flex-col gap-4 p-4 bg-white rounded-lg border border-gray-300 w-full max-w-md">
         <SearchForm initialQuery={initialQuery} onSearch={handleSearch} />
 
