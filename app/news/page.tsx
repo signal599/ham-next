3c71034bde9a2e4b682af898ff4a291a572ce5d2
Ross Keatinge <ross@earthbubble.com>
2026-04-19 17:30:56 -0400

    News content

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -16,16 +16,19 @@ export default function Page() {
         and PHP for many years so this is a new experience for me. Next.js is a
         combination of <a href="http://nodejs.org">Node.js</a> on the backend
         and <a href="https://react.dev">React</a> on the frontend and lots of
-        magic to make it all work together. Most of the backend is still
-        happening in Drupal but I intend to move more of it into NextJS as time
-        permits. A friend named Claude is providing some help along the way. <span className="text-2xl">🙂</span>
+        magic to make it all work together. The language is TypeScript. Most of
+        the backend is still happening in Drupal but I intend to move more of it
+        into NextJS as time permits. A friend named Claude is providing some
+        help along the way. <span className="text-2xl">🙂</span>
       </p>
       <p>
         I’m sure it’s not perfect so let me know if you find any problems. I can
         put the old Drupal version back quickly if there are any showstoppers.
       </p>
       <p>
-        The code is still open source <a href="https://github.com/signal599/ham-next">here</a>. It is both an exciting and scary time to be web developer.
+        The code is still open source{" "}
+        <a href="https://github.com/signal599/ham-next">here</a>. It is both an
+        exciting and scary time to be web developer.
       </p>
       <p>
         73
