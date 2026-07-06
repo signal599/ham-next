e82a9d43c9d457f3058d7fd88a0bfc87cdd4cf12
Ross Keatinge <ross@earthbubble.com>
2026-07-06 11:26:16 -0400

    News updates

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <p>
         The whole site is now written in TypeScript. It has been rewritten and migrated from Drupal which is in PHP. There are now two applications. <a href="https://github.com/signal599/ham-next">Ham-next</a> is the Next.js web application
         that you are looking at now. <a href="https://github.com/signal599/haminfo-cli">HamInfo-cli</a> is a Node.js cron driven
-        app that handles importing data from the FCC, geocoding and data exports. It’s running on a low cost virtual server at Hetzner.
+        app that handles importing data from the FCC, geocoding and data exports. It’s running on a low cost virtual server at Hetzner. I’m feeling pretty happy with the end result.
       </p>
       <p>
         It’s been a fun project and learning experience for me. Most of it is hand coded but I have made use of Claude Code
