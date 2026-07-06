dee980fb06d51a999b473260b929907cd03d1ec1
Ross Keatinge <ross@earthbubble.com>
2026-07-05 20:55:10 -0400

    News update

--- components/news-nav.tsx
+++ components/news-nav.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 
 export default function NewsNav() {
   const links = [
+    { href: "/news/2026-07", title: "July 2026" },
     { href: "/news", title: "May 2026" },
     { href: "/news/2026-04", title: "April 2026" },
     { href: "/news/2025-07", title: "July 2025" },
