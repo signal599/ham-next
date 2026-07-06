aeb60f38d0f0c454c9b0e200b8838c4e96d3d2a7
Ross Keatinge <ross@earthbubble.com>
2026-07-06 11:05:33 -0400

    News update

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -10,10 +10,15 @@ export default function Page() {
         The whole site is now written in TypeScript. It has been rewritten and migrated from Drupal which is in PHP. There are now two applications.
         <a href="https://github.com/signal599/ham-next">Ham-next</a> is the Next.js web application
         that you are looking at now. <a href="https://github.com/signal599/haminfo-cli">HamInfo-cli</a> is a Node.js command line driven
-        app that handles importing data from the FCC and the geocoding. It's running on a low cost virtual server at Hetzner.
+        app that handles importing data from the FCC and the geocoding. It’s running on a low cost virtual server at Hetzner.
       </p>
       <p>
-        It's been a fun project and learning experience for me. Let me know if you find any issues.
+        It’s been a fun project and learning experience for me. Most of it is hand coded but I have made use of Claude Code
+        in a few places which has been a big time saver. I don’t know where the whole AI coding trend is going but I am finding it to be
+        a very useful tool when used carefully.
+      </p>
+      <p>
+        Let me know if you find any issues.
       </p>
       <p>
         73
